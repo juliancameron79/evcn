27f539794f74bcce905d01ef69eb56ab6530ac11
juliancameron79 <juliancameron79@gmail.com>
2020-03-26 12:34:46 +0100

    Ready for review

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,47 +2,47 @@ var map;
 var service;
 
 function handleSearchResults(results, status) {
-	{
-		for (var i = 0; i < results.length; i++) {
-			var marker = new google.maps.Marker({
-				position: results[i].geometry.location,
-				animation: google.maps.Animation.DROP,
-				map: map,
-				icon: 'assets/images/evcs-pinlet-2-medium.png'
-			});
-		}
-	}
+  for (var i = 0; i < results.length; i++) {
+    var marker = new google.maps.Marker({
+      position: results[i].geometry.location,
+      animation: google.maps.Animation.DROP,
+      map: map,
+      icon: 'assets/images/evcs-pinlet-2-medium.png'
+    });
+  }
 }
 
+// Performs a nearby search.
 function performSearch() {
-	var request = {
-		bounds: map.getBounds(),
-		name: ['electric', 'vehicle', 'charging', 'station']
-	};
-	service.nearbySearch(request, handleSearchResults);
+  var request = {
+    bounds: map.getBounds(),
+    name: ['electric', 'vehicle', 'charging', 'station']
+  };
+  service.nearbySearch(request, handleSearchResults);
 }
 
+// Creates the map.
 function initialise(location) {
-	var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+  var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
 
-	var mapOptions = {
-		center: currentLocation,
-		zoom: 12,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
+  var mapOptions = {
+    center: currentLocation,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-	var marker = new google.maps.Marker({
-		position: currentLocation,
-		map: map
-	});
+  var marker = new google.maps.Marker({
+    position: currentLocation,
+    map: map
+  });
 
-	service = new google.maps.places.PlacesService(map);
+  service = new google.maps.places.PlacesService(map);
 
-	google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+  google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
 }
 
 $(document).ready(function() {
-	navigator.geolocation.getCurrentPosition(initialise);
+  navigator.geolocation.getCurrentPosition(initialise);
 });
